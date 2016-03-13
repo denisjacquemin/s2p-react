596c84b4442db4563f9ece43dc38a9ce4ece3e0d
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-03-13 09:06:31 +0100

    removing unused item in menu

--- src/components/header.js
+++ src/components/header.js
@@ -4,12 +4,7 @@ var Header = React.createClass({
   render: function() {
     return (
       <header className="mdl-layout__header is-casting-shadow">
-        <div className="mdl-layout__header-row">
-          <div className="mdl-layout-spacer"></div>
-          <nav className="mdl-navigation mdl-layout--large-screen-only">
-            <a className="mdl-navigation__link" href="">Link</a>
-          </nav>
-        </div>
+
       </header>
     );
   }
