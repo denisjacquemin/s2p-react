7653a3cf5c159ef9cceb1e87e6b544fe38ff04b4
Denis Jacquemin <denis.jacquemin@gmail.com>
2016-04-09 01:40:40 +0200

    removing logs

--- src/containers/full-message.js
+++ src/containers/full-message.js
@@ -4,11 +4,9 @@ import { toggleImportant, showMessagesScreen } from '../actions'
 import FullMessageComponent from '../components/full-message-component'
 
 const mapStateToProps = (state) => {
-  console.log('Message id: ' + state.currentMessage.id)
   let message = state.messages.items.find((m) => {
     return m.id === state.currentMessage.id
   })
-  console.log('Message: ' + JSON.stringify(message))
   return {
     currentMessage: message
   };
